093dfb2778ddb6ff1d2eff2a0b83aca27002aa92
TSHIBANGU Kevin <kevintshib08@gmail.com>
2023-03-08 12:00:41 +0100

    interface commander

---
+++ serveur/Pages/Commander.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using DataModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace serveur.Pages
+{
+    public class CommanderModel : PageModel
+    {
+        public IList<Table> Tables { get; set; }
+        [BindProperty]
+        public IList<Article> Articles { get; set; }
+
+        private readonly DBContext _context;
+
+     
+        public CommanderModel(DBContext context)
+        {
+            _context = context;
+        }
+
+
+
+        public async Task OnGetAsynch()
+        {
+
+            Articles = await _context.Articles.ToListAsync();
+            Tables = _context.Tables.ToList();
+        }
+    }
+}
