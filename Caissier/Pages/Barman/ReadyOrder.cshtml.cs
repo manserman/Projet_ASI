6b904c57f65d6c8454eaff585c39d2efa4cf6e39
TSHIBANGU Kevin <kevintshib08@gmail.com>
2023-03-09 05:45:09 +0100

    Intégration de l'interface Barman et modif sur la prise de commande

---
+++ Caissier/Pages/Barman/ReadyOrder.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjetASI.Pages.Barman
+{
+    public class ReadyOrderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
