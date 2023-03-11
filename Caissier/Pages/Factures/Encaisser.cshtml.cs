42dc47a2ba074e5ffe59ab6268e489b5a10767b5
TSHIBANGU Kevin <kevintshib08@gmail.com>
2023-03-11 18:23:27 +0100

    Intégration de l'interface des factures pour l'admin

---
+++ Caissier/Pages/Factures/Encaisser.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjetASI.Pages.Factures
+{
+    public class EncaisserModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
